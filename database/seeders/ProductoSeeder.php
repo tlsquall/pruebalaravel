dbc1d3a34db5daff356565b013ba3bc2f9d81b20
TLSquall <squallruiz@gmail.com>
2022-10-13 09:23:32 -0400

    actualizacion de seed

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -17,14 +17,32 @@ class ProductoSeeder extends Seeder
     {
         DB::table('productos')->insert([
             'id' => '1',
-            'nombre' => 'Leche',
-            'precio' => '100.00',
-            'impuesto' => '12.00'
+            'nombre' => 'Producto 1',
+            'precio' => '123.45',
+            'impuesto' => '5.00'
         ]);
         DB::table('productos')->insert([
             'id' => '2',
-            'nombre' => 'Carne',
-            'precio' => '50.00',
+            'nombre' => 'Producto 2',
+            'precio' => '45.65',
+            'impuesto' => '15.00'
+        ]);
+        DB::table('productos')->insert([
+            'id' => '3',
+            'nombre' => 'Producto 3',
+            'precio' => '39.73',
+            'impuesto' => '12.00'
+        ]);
+        DB::table('productos')->insert([
+            'id' => '4',
+            'nombre' => 'Producto 4',
+            'precio' => '250.00',
+            'impuesto' => '8.00'
+        ]);
+        DB::table('productos')->insert([
+            'id' => '5',
+            'nombre' => 'Producto 5',
+            'precio' => '59.35',
             'impuesto' => '10.00'
         ]);
     }
